3d3e5e7b2e6c101c4fd49c410bdcfb3c43e6eaae
Daniel Frank <daniel.frank@ipvs.uni-stuttgart.de>
2023-03-03 23:38:06 +0100

    add plotting for state, input and outputs

--- src/hybridstablemodel/example/plot_cartpole_states.py
+++ src/hybridstablemodel/example/plot_cartpole_states.py
@@ -1,24 +1,28 @@
 from hybridstablemodel.model.statespace import ContinuousNonlinear
 from hybridstablemodel.simulator import Simulator
 from hybridstablemodel.system.cartpole import CartPole
-from hybridstablemodel.plot.plot_simulation_results import plot_states
-
+from hybridstablemodel.plot.plot_simulation_results import plot_states, plot_outputs, plot_inputs
+import matplotlib.pyplot as plt
 
 import numpy as np
 
 if __name__ == "__main__":
-    system = CartPole()
+    system = CartPole(mu_c=0.0, mu_p=0.1)
     model = ContinuousNonlinear(
         f=system.state_dynamics, g=system.output_function, nx=4, nu=1, ny=1
     )
     T_end = 10.0
-    step_size = 1.0
-    N = int(T_end)
+    step_size = 0.02
+    N = int(T_end/step_size)
     sim = Simulator(T=T_end, step_size=step_size)
-    u = [np.array([[u]]) for u in np.random.rand(N)]
+    us = [np.array([[u]]) for u in np.random.uniform(-2,2,size=(N,))]
     result, t, _ = sim.simulate(
         model=model,
         initial_state=np.array([[0], [0], [np.pi + 0.1], [0]]),
-        input=u,
+        input=us,
     )
     plot_states(result=result, t=t)
+    plot_inputs(result=result, t=t)
+    plot_outputs(result=result, t=t)
+    plt.show()
+    
--- src/hybridstablemodel/plot/plot_simulation_results.py
+++ src/hybridstablemodel/plot/plot_simulation_results.py
@@ -3,11 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
 
+plt.rcParams['text.usetex'] = True
 
 def plot_states(result: SimulationResult, t: NDArray[np.float64]) -> None:
     nx = result.xs[0].shape[0]
+    fig, axs = plt.subplots(nrows=nx, ncols=1, tight_layout=True, squeeze=False)
+    fig.suptitle('State plots')
+    for element, ax in zip(range(nx), axs[:, 0]):
+        ax.plot(t, np.array([x[element] for x in result.xs]))
+        ax.set_title(f'$x_{element+1}$')
+        ax.grid()
 
-    for element in range(nx):
-        plt.plot(t, np.array([x[element] for x in result.xs]))
+def plot_inputs(result: SimulationResult, t: NDArray[np.float64]) -> None:
+    nu = result.us[0].shape[0]
+    fig, axs = plt.subplots(nrows=nu, ncols=1, tight_layout=True, squeeze=False)
+    fig.suptitle('Input plots')
+    for element, ax in zip(range(nu), axs[:, 0]):
+        u = np.array([u[element] for u in result.us])
+        ax.plot(t, np.array([u[element] for u in result.us]))
+        ax.set_title(f'$u_{element+1}$')
+        ax.grid()
+    
 
-    plt.show()
+def plot_outputs(result: SimulationResult, t: NDArray[np.float64]) -> None:
+    ny = result.ys[0].shape[0]
+    fig, axs = plt.subplots(nrows=ny, ncols=1, tight_layout=True, squeeze=False)
+    fig.suptitle('Output plots')
+    for element, ax in zip(range(ny), axs[:, 0]):
+        ax.plot(t, np.array([y[element] for y in result.ys]))
+        ax.set_title(f'$y_{element+1}$')
+        ax.grid()
